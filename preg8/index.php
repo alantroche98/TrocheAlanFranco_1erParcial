79990a34111b160a74ebe94d84adf0820ac65b14
alantroche98 <61810934+alantroche98@users.noreply.github.com>
2024-10-04 23:05:08 -0400

    Pregunta 8
    8. Realice un programa en PHP que imprima “Hola mundo”

---
+++ preg8/index.php
@@ -0,0 +1,8 @@
+
+
+<?php
+    echo "<h1 style='font-size: 200px; 
+	                 color: blue; 
+					 font-family: Arial, sans-serif; 
+					 background-color:#f0f8ff;'>    Hola mundo</h1>";
+?>
